a7ad1b8631e56f35769b8e8205e219d5d4eef30f
Denys Vysockij <131520948+denyxqq@users.noreply.github.com>
2024-10-13 23:04:38 +0300

    Add files via upload

---
+++ test.js
@@ -0,0 +1,62 @@
+ let farengeyt;  
+ const celsius = 10;
+farengeyt = celsius * 1.8 + 32;
+console.log(farengeyt);
+
+// ______________________________________
+
+const day = 30;
+const hour = day * 24;
+const minute = hour * 60;
+console.log(hour);
+console.log(minute);
+
+// -------------------------------
+
+let heath = 100;
+let energy = 100;
+heath -= 20;
+energy -= 50;
+console.log(heath);
+console.log(energy);
+
+// ------------------------------------
+
+let cost = 1337;
+let sale = cost / 100 * 10;
+cost -= sale;
+console.log(cost);
+
+// --------------------------
+
+let number = 7.952812;
+number = Math.floor(number);
+console.log(number);
+
+// _________________________________
+
+let number2 = "7.95281222485";
+number2= parseFloat(number2);
+console.log(number2);
+
+// ____________________________
+
+let number3 = "394";
+number = parseInt(number3);
+console.log(number3);
+
+// ____________________________________
+
+let number4 = "394";
+number4 = Math.sqrt(number4);
+console.log(number4);
+
+// ____________________________________
+
+let oneTwo;
+let string;
+oneTwo = parseInt("52");
+console.log(oneTwo);
+string = oneTwo.toString();
+console.log(string);
+
